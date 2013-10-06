402e30699217e6a7d83829a3330c8e32679cde82
Ma Lin <malin@ubuntu.ubuntu-domain>
2013-10-06 20:54:24 +0800

    modified for merge

--- kern/monitor.c
+++ kern/monitor.c
@@ -232,11 +232,6 @@ monitor(struct Trapframe *tf)
 	cprintf("Welcome to the JOS kernel monitor!\n");
 	cprintf("Type 'help' for a list of commands.\n");
 
-	unsigned int i = 0x00646c72;
-    	cprintf("H%x Wo%s", 57616, &i);
-
-	cprintf("\n%r001%s %r002%s %r012%s %r005%s\n", "This", "is", "CHALLENGE", "!");
-
 	while (1) {
 		buf = readline("K> ");
 		if (buf != NULL)
